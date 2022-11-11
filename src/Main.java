501a34c3bb40aeb7aaa8a437a3142730d1441925
Denis <1994osin@gmail.com>
2022-11-11 15:57:21 +0300

    Сделанное ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Task 1
+        int now_a_Year = 2022;
+        int twoHundredYearsBeforeThisYear = now_a_Year - 200;
+        int afterTheHundredthYearOfTheCurrentYear = now_a_Year + 100;
+        for (int i = 0; i <= afterTheHundredthYearOfTheCurrentYear; i += 79) {
+            if (i >= twoHundredYearsBeforeThisYear)
+                System.out.println(i);
+        }
+        System.out.println();
+
+        // Task 2
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(2 + " * " + i + " = " + (2 * i));
+        }
+
     }
 }
